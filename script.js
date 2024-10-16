a82a7ff9e8255aaba78eda2f77612dfbee430799
simodevjs <simodevjs@gmail.com>
2024-10-16 04:02:36 +0100

    aaa

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
